a5bcbe9e51e12da3fd9a23bcbd93ce2ef91280e0
Monu <Monu@Mahesh-1802>
2022-09-07 12:26:58 +0530

    commit

---
+++ ShiftOp/src/ShiftOp.java
@@ -0,0 +1,13 @@
+
+public class ShiftOp 
+{
+	public static void main(String[] args) 
+	{
+		int a= 10;
+        System.out.println(a<<2);
+        System.out.println(a>>3);
+        System.out.println(a<<1);
+        
+	}
+
+}
