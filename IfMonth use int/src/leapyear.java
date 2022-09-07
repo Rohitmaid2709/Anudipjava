28a7218261331a33a701734cc585d2f802784b3d
Monu <Monu@Mahesh-1802>
2022-09-07 12:26:10 +0530

    commit

---
+++ IfMonth use int/src/leapyear.java
@@ -0,0 +1,14 @@
+
+public class leapyear 
+{
+	
+		public static void main(String[] args) 
+		{
+		    int year=2022;
+	        if (year % 4 == 0)
+		    	 System.out.println("Specified year is a leap year");
+		   else
+		        System.out.println("Specified year is not a leap year");
+		
+	 }	
+}
