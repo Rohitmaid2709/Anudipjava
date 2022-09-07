8f40f2aadb11021e0927730859d51d5d6c22871a
Monu <Monu@Mahesh-1802>
2022-09-07 12:13:23 +0530

    commit

---
+++ NestedIf Project/src/NestedIfProject.java
@@ -0,0 +1,24 @@
+
+public class NestedIfProject {
+
+	public static void main(String[] args) 
+	{
+		  int a=30;
+	        int b=150;
+	        int c=100;
+	        if (a>b)
+	        {
+	           if(a>c)
+	           System.out.println("a is greatest Number");
+	            else
+	            System.out.println(" c is greatest Number"); 
+	          }
+	             else if (b>c)
+	                 System.out.println(" b is greatest Number"); 
+	              else
+	                    System.out.println(" c is greatest Number"); 
+	          
+
+	}
+
+}
