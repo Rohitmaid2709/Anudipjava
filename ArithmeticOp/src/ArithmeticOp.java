1439a2592019c16a5e4340029dba18ef445d71a9
Monu <Monu@Mahesh-1802>
2022-09-07 12:17:50 +0530

    commit

---
+++ ArithmeticOp/src/ArithmeticOp.java
@@ -0,0 +1,21 @@
+
+public class ArithmeticOp 
+{
+	public static void main(String[] args) 
+	{
+		 int n1=20;
+	        int n2=10;
+	        int result;
+	        result=n1+n2;
+	        System.out.println("Reult :"+(n1+n2));
+	       result=n1-n2;
+	        System.out.println("substraction :"+result);
+	        result=n1*n2;
+	        System.out.println("multiplication :"+result);
+	        result=n1/n2;
+	        System.out.println("division :"+result);
+	        result=n1%n2;
+	        System.out.println("Remainder:"+result);
+	        }
+
+}
