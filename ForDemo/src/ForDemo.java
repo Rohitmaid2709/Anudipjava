dd1af27e90e1254e819a7ba70595e7efb10c6993
Monu <Monu@Mahesh-1802>
2022-09-07 12:25:45 +0530

    commit

---
+++ ForDemo/src/ForDemo.java
@@ -0,0 +1,17 @@
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		
+			int Num=2;
+			for(int i=1;i<=10;++i)
+			{
+				System.out.println(Num+" * "+i+" = "+Num*i);	
+			 
+	     	}
+			 
+		}
+
+	
+
+}
