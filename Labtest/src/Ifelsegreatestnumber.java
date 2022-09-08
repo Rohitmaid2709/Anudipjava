eea0e6e3405ee4cfebc8fdfe572e5c2ccfae8066
Monu <Monu@Mahesh-1802>
2022-09-08 12:05:15 +0530

    labtest1

---
+++ Labtest/src/Ifelsegreatestnumber.java
@@ -0,0 +1,24 @@
+
+public class Ifelsegreatestnumber 
+{
+	 public static void main(String[] args) 
+     {
+      int a=22;
+      int b=28;
+      int c=90;
+      if (a>b&&a>c)
+      {
+        System.out.println("a is gretest");
+        }
+       else if (b>a&&b>c)
+       {
+           System.out.println("b is gretest");
+        }
+       else if (c>a&&c>b)
+      {
+      System.out.println("c is gretest");
+      }
+      }
+	
+
+}
