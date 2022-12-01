c901b2b90d3590f3d248d78b169a9782948e36f9
Monu <Monu@Mahesh-1802>
2022-12-01 12:31:40 +0530

    Labtest8

---
+++ Labtest 8/src/Problem2/GetFiles.java
@@ -0,0 +1,26 @@
+package Problem2;
+
+
+import java.io.File;
+
+public class GetFiles {
+
+	public static void main(String[] args) {
+		File file=new File("D:\\IOStream");
+		String[] s=file.list();
+		System.out.println("File names present in the folder");
+		for (String string : s) {
+			System.out.println(string);
+		}
+		System.out.println("\nPermission check:");
+		File[] f =file.listFiles();
+		for (File file2 : f) {
+			
+			System.out.println("File Name : "+file2.getName());
+			System.out.println("Writable : "+file2.canWrite());
+			System.out.println("Readable : "+file2.canRead());
+			System.out.println("Executable : "+file2.canExecute()+"\n");
+		}
+	}
+
+}
---
+++ Labtest 8/src/Problem3/Main.java
@@ -0,0 +1,44 @@
+package Problem3;
+
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.regex.Pattern;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		//creating objects to a student class
+		Student a=new Student("Anuj","1","9307515220");
+		Student b=new Student("Sagar","2","8530895623");
+		Student c=new Student("Adarsh","3","8830566816");
+		
+		//creating a file
+		File f=new File("Student");
+		FileWriter fw=new FileWriter(f);
+		FileReader fr=new FileReader(f);
+		
+		//writing objects into a file
+		fw.write("a");
+		fw.write("b");
+		fw.write("c");
+		//System.out.println(fr.read());
+		
+		//closing the reader and writer streams
+		fw.close();
+		fr.close();
+		
+		System.out.println("\nStudent names starts with 'A' are..\n");
+		Student s[]= {a,b,c};
+		for(int i=0;i<s.length;i++)
+		{
+		   if(Pattern.matches("[A]\\w+",s[i].getName()))
+		    {
+				System.out.println("Name : "+s[i].getName());
+				System.out.println("Student Id : "+s[i].getId());
+				System.out.println("Contact no. : "+s[i].getContact()+"\n");
+		    }
+		}
+	}
+}
